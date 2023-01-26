cb86f5c131ec8f90fca78f80636c95bad33494ea
serenakang <serenakang77@gmail.com>
2023-01-26 00:29:46 -0500

    Added bootstrap buttons and background img for header

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,10 +1,23 @@
-import { Link } from "react-router-dom"
+import menuIcon from "../assets/menu-icon.png"
+import Button from "react-bootstrap/Button"
 
 export default function PageHeader() {
   return (
     <>
       <header className='header'>
-        <h1>ARENTA</h1>
+        <div className='headerLeft'>
+          <h1>ARENTA</h1>
+          <a href=''>
+            <img src={menuIcon} alt='menuIcon' class='menuIcon' />
+          </a>
+        </div>
+        <div className='headerRight'>
+          <a href=''>Log in</a>
+          <div className='headerRightButton'>
+            <Button variant='outline-danger'>Sign Up</Button>{" "}
+            <Button variant='outline-danger'>Rent your car</Button>{" "}
+          </div>
+        </div>
       </header>
     </>
   )
