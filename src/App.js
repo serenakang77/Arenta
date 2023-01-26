6b236b11deed9578298b3af592d5c85fc18f16ff
serenakang <serenakang77@gmail.com>
2023-01-25 23:47:06 -0500

    Added basic structure and pageheader components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// import axios from "axios"
+import "./styles/styles.css"
+import PageHeader from "./components/PageHeader"
+// import { Routes, Route } from "react-router-dom"
 
 function App() {
+  // const options = {
+  //   method: "GET",
+  //   url: "https://booking-com.p.rapidapi.com/v1/car-rental/search",
+  //   params: {
+  //     sort_by: "recommended",
+  //     drop_off_latitude: "50.08773",
+  //     pick_up_latitude: "50.08773",
+  //     from_country: "it",
+  //     pick_up_longitude: "14.421133",
+  //     currency: "CAD",
+  //     drop_off_datetime: "2023-04-17 13:00:00",
+  //     pick_up_datetime: "2023-04-16 13:00:00",
+  //     drop_off_longitude: "14.421133",
+  //     locale: "en-gb",
+  //   },
+  //   headers: {
+  //     "X-RapidAPI-Key": "d68a0da33dmshec23235f282deeap141e2bjsn8069a44d4671",
+  //     "X-RapidAPI-Host": "booking-com.p.rapidapi.com",
+  //   },
+  // }
+
+  // axios
+  //   .request(options)
+  //   .then(function (response) {
+  //     console.log(response.data)
+  //   })
+  //   .catch(function (error) {
+  //     console.error(error)
+  //   })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <PageHeader />
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/PageHeader.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export default function PageHeader() {
+  return (
+    <>
+      <header className='header'>
+        <h1>ARENTA</h1>
+      </header>
+    </>
+  )
+}
